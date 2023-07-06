8a5924c10e715d313c4ab8b21a6ff66dcc5d3f51
Ultimate Samarth Sharma <68366492+UltimateSamarthSharma@users.noreply.github.com>
2023-07-06 19:59:28 +0530

    Q1.js

--- Walkthrough with Express Assignment/Q1.js
+++ Walkthrough with Express Assignment/Q1.js
@@ -5,27 +5,27 @@ const http = require('http');
 const menProducts = [
   { id: 1, name: 'Men Product 1', category: 'Men' },
   { id: 2, name: 'Men Product 2', category: 'Men' },
-  { id: 3, name: 'Men Product 2', category: 'Men' },
-  { id: 4, name: 'Men Product 2', category: 'Men' },
-  { id: 5, name: 'Men Product 2', category: 'Men' },
-  { id: 6, name: 'Men Product 2', category: 'Men' },
-  { id: 7, name: 'Men Product 2', category: 'Men' },
-  { id: 8, name: 'Men Product 2', category: 'Men' },
-  { id: 9, name: 'Men Product 2', category: 'Men' },
-  { id: 10, name: 'Men Product 2', category: 'Men' },
+  { id: 3, name: 'Men Product 3', category: 'Men' },
+  { id: 4, name: 'Men Product 4', category: 'Men' },
+  { id: 5, name: 'Men Product 5', category: 'Men' },
+  { id: 6, name: 'Men Product 6', category: 'Men' },
+  { id: 7, name: 'Men Product 7', category: 'Men' },
+  { id: 8, name: 'Men Product 8', category: 'Men' },
+  { id: 9, name: 'Men Product 9', category: 'Men' },
+  { id: 10, name: 'Men Product 10', category: 'Men' },
 ];
 
 const womenProducts = [
   { id: 1, name: 'Women Product 1', category: 'Women' },
   { id: 2, name: 'Women Product 2', category: 'Women' },
-  { id: 3, name: 'Women Product 2', category: 'Women' },
-  { id: 4, name: 'Women Product 2', category: 'Women' },
-  { id: 5, name: 'Women Product 2', category: 'Women' },
-  { id: 6, name: 'Women Product 2', category: 'Women' },
-  { id: 7, name: 'Women Product 2', category: 'Women' },
-  { id: 8, name: 'Women Product 2', category: 'Women' },
-  { id: 9, name: 'Women Product 2', category: 'Women' },
-  { id: 10, name: 'Women Product 2', category: 'Women' },
+  { id: 3, name: 'Women Product 3', category: 'Women' },
+  { id: 4, name: 'Women Product 4', category: 'Women' },
+  { id: 5, name: 'Women Product 5', category: 'Women' },
+  { id: 6, name: 'Women Product 6', category: 'Women' },
+  { id: 7, name: 'Women Product 7', category: 'Women' },
+  { id: 8, name: 'Women Product 8', category: 'Women' },
+  { id: 9, name: 'Women Product 9', category: 'Women' },
+  { id: 10, name: 'Women Product 10', category: 'Women' },
 ];
 
 const server = http.createServer((req, res) => {
